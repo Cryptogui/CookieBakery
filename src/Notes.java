657841c9d109aa8a148ca1cebda25dd7a6f31848
MexicanBull <balder.hernandez@gmail.com>
2017-10-01 17:27:16 +0300

    coooomment

--- src/Notes.java
+++ src/Notes.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Notes {
 	ArrayList<ArrayList<Integer>> notes = new ArrayList<>();
 	
+	//define number of frets for program to handle in parameters
 	public Notes(int frets){
 		for(int i=0; i < 6; i++){
 			notes.add(new ArrayList<Integer>());
