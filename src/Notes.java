f02cdd01eb250025d2b723d0548472deba07a53a
MexicanBull <balder.hernandez@gmail.com>
2017-10-03 19:58:30 +0300

    getNote function added

--- src/Notes.java
+++ src/Notes.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Notes {
 	//ArrayList<ArrayList<Integer>> notes = new ArrayList<>();
 	int [][] notes;
+	private Random rand = new Random();
 	
 	//define number of frets for program to handle in parameters
 	public Notes(int frets){
@@ -21,5 +22,24 @@ public class Notes {
 				notes[i][k] = k;
 			}
 		}
+	}
+	//both frets & strings
+	public Notes(int strings, int frets){
+		this.notes = new int[strings][frets+1];
+		for(int i=0; i<notes.length; i++){
+			for(int k=0; k<notes[i].length; k++){
+				notes[i][k] = k;
+			}
+		}
+	}
+	
+	//random note, no specification
+	public int[] getNote(){
+		int string = rand.nextInt(notes.length);
+		int min = 0;
+		int max = this.notes[string].length;
+		int fret = rand.nextInt(max-min)+min;
+		int[] note = {string,fret};
+		return note;
 	}
 }
