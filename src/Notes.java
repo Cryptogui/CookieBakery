a1f04adef75b4fe8aa63d645cdc0f31413435b67
MexicanBull <balder.hernandez@gmail.com>
2017-10-03 19:28:20 +0300

    ändrad notlista

--- src/Notes.java
+++ src/Notes.java
@@ -2,14 +2,23 @@
 import java.util.*;
 
 public class Notes {
-	ArrayList<ArrayList<Integer>> notes = new ArrayList<>();
+	//ArrayList<ArrayList<Integer>> notes = new ArrayList<>();
+	int [][] notes;
 	
 	//define number of frets for program to handle in parameters
 	public Notes(int frets){
+		/*
 		for(int i=0; i < 6; i++){
 			notes.add(new ArrayList<Integer>());
 			for(int k=0; k <= frets; k++){
 				notes.get(i).add(k);
+			}
+		}
+		*/
+		this.notes = new int[6][frets+1];
+		for(int i=0; i<notes.length; i++){
+			for(int k=0; k<notes[i].length; k++){
+				notes[i][k] = k;
 			}
 		}
 	}
--- src/RiffGen.java
+++ src/RiffGen.java
@@ -3,8 +3,15 @@ import java.util.ArrayList;
 public class RiffGen {
 	public static void main(String[]args){
 		Notes fretboard = new Notes(3);
-		for(ArrayList i: fretboard.notes){
+		/*
+		for(ArrayList i: fretboard.notes.){
 			for (int k=0; k < i.size(); k++){
+				System.out.println(k);
+			}
+		}
+		*/
+		for(int i=0; i<fretboard.notes.length; i++){
+			for(int k: fretboard.notes[i]){
 				System.out.println(k);
 			}
 		}
