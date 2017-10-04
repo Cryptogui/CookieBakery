d8d2d8c7d703e57f2d557576ec969c55d58c1bd4
MexicanBull <balder.hernandez@gmail.com>
2017-10-04 11:44:57 +0300

    getNote variationer

---
+++ src/Fretboard.java
@@ -0,0 +1,97 @@
+//note handling
+import java.util.*;
+
+public class Fretboard {
+	//ArrayList<ArrayList<Integer>> notes = new ArrayList<>();
+	int [][] notes;
+	private Random rand = new Random();
+	int strings, frets;
+	
+	//define number of frets for program to handle in parameters
+	public Fretboard(int fretNum){
+		/*
+		for(int i=0; i < 6; i++){
+			notes.add(new ArrayList<Integer>());
+			for(int k=0; k <= frets; k++){
+				notes.get(i).add(k);
+			}
+		}
+		*/
+		frets = fretNum;
+		strings = 6;
+		this.notes = new int[strings][frets+1];
+		for(int i=0; i<notes.length; i++){
+			for(int k=0; k<notes[i].length; k++){
+				notes[i][k] = k;
+			}
+		}
+	}
+	//both frets & strings
+	public Fretboard(int stringNum, int fretNum){
+		frets = fretNum;
+		strings = stringNum;
+		this.notes = new int[strings][frets+1];
+		for(int i=0; i<notes.length; i++){
+			for(int k=0; k<notes[i].length; k++){
+				notes[i][k] = k;
+			}
+		}
+	}
+	
+	int min, max, string;
+	//random note, no specification
+	public int[] getNote(){
+		string = rand.nextInt(notes.length);
+		min = 0;
+		max = this.notes[string].length;
+		int fret = rand.nextInt(max-min)+min;
+		int[] note = {string,fret};
+		return note;
+	}
+	//random note between fret minimum and maximum, random string
+	public int[] getNote(int minimum, int maximum){
+		string = rand.nextInt(notes.length);
+		if(minimum<0){
+			min = 0;
+		} else if(minimum>this.notes[string].length) {
+			min = this.notes[string].length;
+		} else {
+			min = minimum;
+		}
+		if(maximum>this.notes[string].length){
+			max = this.notes[string].length;
+		} else if(maximum<min) {
+			max = min;
+		} else {
+			max = maximum;
+		}
+		int fret = rand.nextInt(max-min)+min;
+		int[] note = {string,fret};
+		return note;
+	}
+	
+	public int[] getNote(int minimum, int maximum, int selectedString){
+		if(0<=selectedString && selectedString<= this.notes.length){
+			string = selectedString;
+		} else {
+			string = rand.nextInt(notes.length);
+		}
+		if(minimum<0){
+			min = 0;
+		} else if(minimum>this.notes[string].length) {
+			min = this.notes[string].length;
+		} else {
+			min = minimum;
+		}
+		if(maximum>this.notes[string].length){
+			max = this.notes[string].length;
+		} else if(maximum<min) {
+			max = min;
+		} else {
+			max = maximum;
+		}
+		int fret = rand.nextInt(max-min)+min;
+		int[] note = {string,fret};
+		return note;
+	}
+}
--- src/RiffGen.java
+++ src/RiffGen.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class RiffGen {
 	public static void main(String[]args){
-		Notes fretboard = new Notes(3);
+		Fretboard fretboard = new Fretboard(3);
 		/*
 		for(ArrayList i: fretboard.notes.){
 			for (int k=0; k < i.size(); k++){
